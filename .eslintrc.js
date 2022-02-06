93274819c38d3e5eb056548a5e1413cfa1d2c0a9
Alexander Allen <richard.alexander.allen@gmail.com>
2022-02-06 16:55:02 -0500

    Configure eslint for TypeScript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,44 @@
-{
-    "extends": ["airbnb", "airbnb/hooks"],
-    "overrides": [
-      {
-        "files": [
-          "**/*.spec.js",
-          "**/*.spec.jsx",
-          "**/*.test.js",
-          "**/*.test.jsx"
-        ],
-        "env": {
-          "jest": true
-        }
-      }
-    ]
-}
+/**
+ * References:
+ * - https://typescript-eslint.io/docs/linting/
+ */
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+  ],
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended', // removes redundant warnings between TS & ESLint
+        'plugin:@typescript-eslint/recommended', // rules specific to typescript, e.g., writing interfaces
+        'airbnb-typescript',
+      ],
+    },
+    // Override enviornment for test files.
+    {
+      files: [
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+        '**/*.test.js',
+        '**/*.test.jsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
