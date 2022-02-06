30aa024dae9745b2cdc96604b6704aafe5be45e6
Alexander Allen <richard.alexander.allen@gmail.com>
2022-02-06 10:03:27 -0500

    Cannot notify from WSL at the moment.

--- tests/node-notify-manual.js
+++ tests/node-notify-manual.js
@@ -1,20 +1,10 @@
-// const NotifySend = require('node-notifier/notifiers/notifysend');
-const isWsl = require('is-wsl');
-
 const cp = require('child_process');
+// const isWsl = require('is-wsl');
 
 function sum(a, b) {
-  // const WindowsToaster = require('node-notifier/notifiers/toaster');
-
-  const options = {
-    message: 'Hello from node, Mr. User!',
-    category: 'Alpine WSL',
-    title: '',
-  };
-
   const ret = new Promise((resolve, reject) => {
-    console.log('looks promising');
-    console.log(`running inside wsl? ${isWsl}`);
+    // console.log('looks promising');
+    // console.log(`running inside wsl? ${isWsl}`);
 
     cp.exec('notify-send "hi"', (
       error,
@@ -25,16 +15,7 @@ function sum(a, b) {
       if (error) reject(error);
       resolve(a + b);
     });
-
-    // new NotifySend().notify(options, (err, res, meta) => {
-    //   console.log({ err });
-
-    //   resolve(a + b);
-    // });
   });
-
-  // resolving to an invalid command
-  //  cmd: 'notify-send "Node Notification:" "Hello from node, Mr. User!" --expire-time "10000"'
 
   return ret;
 }
