a5f7ff2d52ca697d9691213c9f1ab9516c8c6c06
Alexander Allen <richard.alexander.allen@gmail.com>
2022-02-06 09:05:11 -0500

    First async notify test (OK).
    No patches needed on node-notifier, it's all in the WSL notify-send configuration.

--- tests/jest-test.js
+++ tests/jest-test.js
@@ -1,4 +1,41 @@
+// const NotifySend = require('node-notifier/notifiers/notifysend');
+const isWsl = require('is-wsl');
+
+const cp = require('child_process');
+
 function sum(a, b) {
-  return a + b;
+  // const WindowsToaster = require('node-notifier/notifiers/toaster');
+
+  const options = {
+    message: 'Hello from node, Mr. User!',
+    category: 'Alpine WSL',
+    title: '',
+  };
+
+  const ret = new Promise((resolve, reject) => {
+    console.log('looks promising');
+    console.log(`running inside wsl? ${isWsl}`);
+
+    cp.exec('notify-send "hi"', (
+      error,
+      stdout,
+      stderr,
+    ) => {
+      console.log({ stdout, stderr });
+      if (error) reject(error);
+      resolve(a + b);
+    });
+
+    // new NotifySend().notify(options, (err, res, meta) => {
+    //   console.log({ err });
+
+    //   resolve(a + b);
+    // });
+  });
+
+  // resolving to an invalid command
+  //  cmd: 'notify-send "Node Notification:" "Hello from node, Mr. User!" --expire-time "10000"'
+
+  return ret;
 }
 module.exports = sum;
--- tests/jest.test.js
+++ tests/jest.test.js
@@ -1,5 +1,8 @@
-const sum = require('./jest-test');
+import sum from './jest-test';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', async () => {
+  expect.assertions(1);
+  // https://stackoverflow.com/a/54068300/467453
+  const res = await sum(1, 2);
+  expect(res).toBe(3);
 });
